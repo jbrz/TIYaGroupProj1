4e20f792eceacedf9e0dcfb0a7cbcb434a17b458
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-14 17:16:53 -0400

    initial js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,7 @@
 
 // URLs for API data
 
-
-
-
-  // let googleURL = 'http://maps.google.apis.com/maps/api/js?key=bGaods0CxlSXdo5bpDuFnJZVFpU&callback=initMap';
-
-  let flickrURL = 'https://api.flickr.com/services/rest/?method=flickr.test.echo&api_key=ba1b9d0f8d9ba8dc20eadd024c969c34';
+  // let flickrURL = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=food&format=json&api_key=ba1b9d0f8d9ba8dc20eadd024c969c34';
 
   let menuURL = 'https://json-data.herokuapp.com/restaurant/menu/1';
 
@@ -27,25 +22,26 @@
 //     console.log(menuResponse);
 //   });
 
-// let newsPromise = $.getJSON(newsURL);
-//   newsPromise.then( function (newsResponse) {
-//     console.log(newsResponse);
-//   });
+let newsPromise = $.getJSON(newsURL);
+
+  newsPromise.then( function (newsResponse) {
+    console.log(newsResponse);
+    console.log(newsResponse.title);
+    console.log(newsResponse.post);
+  });
+  
 
 let specialPromise = $.getJSON(specialURL);
+
   specialPromise.then( function (specialResponse) {
     console.log(specialResponse);
+    console.log(specialResponse.menu_item_id);
   });
 
-// let googlePromise = $.getJSON(googleURL);
-//   googlePromise.then( function (googleResponse) {
-//     console.log(googleResponse);
+// let flickrPromise = $.getJSON(flickrURL);
+//   flickrPromise.then( function (flickrResponse) {
+//     console.log(flickrResponse);
 //   });
-
-let flickrPromise = $.getJSON(flickrURL);
-  flickrPromise.then( function (flickrResponse) {
-    console.log(flickrResponse);
-  });
 
 
 
