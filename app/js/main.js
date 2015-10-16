0e97b854bc51a1992d6d918c4156788b520c0cfc
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-16 14:19:02 -0400

    andrew edits waiting on master file

--- app/js/main.js
+++ app/js/main.js
@@ -2,71 +2,36 @@
 
 (function () {
   console.log('It Works!');
-
-  // URLs for API data
-
   var tagSearch = "food";
-
   var flickrURL = 'https://api.flickr.com/services/rest?method=flickr.photos.search&tags=' + tagSearch + '&format=json&nojsoncallback=1&api_key=ba1b9d0f8d9ba8dc20eadd024c969c34';
-
   var menuURL = 'https://json-data.herokuapp.com/restaurant/menu/1';
-
   var newsURL = 'https://json-data.herokuapp.com/restaurant/news/1';
-
   var specialURL = 'https://json-data.herokuapp.com/restaurant/special/1';
-
-  // newsBox
-
   var newsPromise = $.getJSON(newsURL);
-
-  // append newsTitle text
   newsPromise.then(function (newsResponse) {
     $('.newsTitle').append(newsResponse.title);
   });
-
-  // append newsPost text
   newsPromise.then(function (newsResponse) {
     $('.newsPost').append(newsResponse.post);
   });
-
-  // menuPromise
-
-  // promise and confirmation
-
   var menuPromise = $.getJSON(menuURL);
   var specialPromise = $.getJSON(specialURL);
-
-  // menu Sections
   menuPromise.then(function (menuResponse) {
-    // specialPromise.then(function (specialResponse) {
-    //   let specialItemId = _.values(_.pick(specialResponse,'menu_item_id'));
-    //   let specialItemIdValue = _.first(specialItemId);
-    // });
-
-    // pulling the name and item array of each section
+    specialPromise.then(function (specialResponse) {
+      var specialItemId = _.values(_.pick(specialResponse, 'menu_item_id'));
+      var specialItemIdValue = _.first(specialItemId);
+    });
+    console.log(menuResponse);
     var menuSections = _.keys(menuResponse);
     console.log(menuSections);
-<<<<<<< HEAD
-=======
     var arrayofArrays = _.values(menuResponse);
     console.log(arrayofArrays);
->>>>>>> 9c1fbd927644669ffda90abaae5f4f8b5e7207d8
-
-    //  Injecting Menu divs
     var menuTemplateString = $('#menuDiv').text();
     var renderTemplate = _.template(menuTemplateString);
-
-    _.each(menuSections, function (SectionTitle) {
-      var menuHTML = renderTemplate(SectionTitle);
+    _.each(menuSections, function (sectionTitle) {
+      var menuHTML = renderTemplate(sectionTitle);
       $('.menu').append(menuHTML);
     });
-
-    //  Menu div injection
-<<<<<<< HEAD
-    var arrayofArrays = _.values(menuResponse);
-    console.log(arrayofArrays);
-=======
->>>>>>> 9c1fbd927644669ffda90abaae5f4f8b5e7207d8
 
     _.each(arrayofArrays, function (array) {
 
@@ -121,7 +86,7 @@
     var sampleOfImages = _.sample(arrayOfImgUrls, numberOfImages);
     // console.log(sampleOfImages);
     _.each(sampleOfImages, function (randomImage) {
-      $('#imageContainer').append("<div class=\"imgContainerBox\">\n              <img class=\"imgBox\" src={randomImage}\n            </div>");
+      $('#imageContainer').append("<div class=\"imgContainerBox\">\n              <img class=\"imgBox\" src={randomImage}</div>");
     });
   });
 })();
--- src/main.js
+++ src/main.js
@@ -1,70 +1,61 @@
 "use strict";
 
 (function () {
+
   console.log('It Works!');
 
-
-// URLs for API data
-
-  
   let tagSearch = "food";
-
   let flickrURL = 'https://api.flickr.com/services/rest?method=flickr.photos.search&tags='+ tagSearch + '&format=json&nojsoncallback=1&api_key=ba1b9d0f8d9ba8dc20eadd024c969c34';
-
   let menuURL = 'https://json-data.herokuapp.com/restaurant/menu/1';
-
   let newsURL = 'https://json-data.herokuapp.com/restaurant/news/1';
-
   let specialURL = 'https://json-data.herokuapp.com/restaurant/special/1';
 
-
-// newsBox
-
   let newsPromise = $.getJSON(newsURL);
-  
-  // append newsTitle text
+
+
   newsPromise.then(function (newsResponse) {
     $('.newsTitle').append(newsResponse.title);
   });
 
-  // append newsPost text
   newsPromise.then(function (newsResponse) {
     $('.newsPost').append(newsResponse.post);
   });
 
 
-// menuPromise
-
-// promise and confirmation
-
   let menuPromise = $.getJSON(menuURL);
   let specialPromise = $.getJSON(specialURL);
 
-// menu Sections
+
   menuPromise.then(function (menuResponse) {
     // specialPromise.then(function (specialResponse) {
     //   let specialItemId = _.values(_.pick(specialResponse,'menu_item_id'));
     //   let specialItemIdValue = _.first(specialItemId);
     // });
+  console.log(menuResponse);
+
+
+  let menuSections = _.keys(menuResponse);
+  console.log(menuSections);
+
+
+  let arrayofArrays = _.values(menuResponse);
+  console.log(arrayofArrays);
+
+
+  let menuTemplateString = $('#menuDiv').text();
+  let renderTemplate = _.template(menuTemplateString);
+
   
-// pulling the name and item array of each section
-    let menuSections = _.keys(menuResponse);
-      console.log(menuSections)
-
-    let arrayofArrays = _.values(menuResponse);
-      console.log(arrayofArrays);
-
-
-//  Injecting Menu divs
-    var menuTemplateString = $('#menuDiv').text();
-    var renderTemplate = _.template(menuTemplateString);
-
-    _.each(menuSections, function (SectionTitle) {
-      var menuHTML = renderTemplate(SectionTitle)
+    _.each(menuSections, function (sectionTitle) {
+      var menuHTML = renderTemplate(sectionTitle);
       $('.menu').append(menuHTML);
     });
 
-//  Menu div injection
+
+    _.each (menuResponse, function (x) {
+      _.each (menuSections, _.pick(x, menuSections))
+
+    })
 
 
     _.each(arrayofArrays, function (array) {
@@ -126,14 +117,12 @@ let flickrPromise = $.getJSON(flickrURL);
     let arrayOfImgUrls = _.map(arrayOfPhotos, function (photo){ 
     return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg"
     });
-    let numberOfImages = 5
+    let numberOfImages = 5;
     let sampleOfImages = _.sample(arrayOfImgUrls, numberOfImages);
     // console.log(sampleOfImages);
     _.each(sampleOfImages, function(randomImage) {
-        $('#imageContainer').append(
-            `<div class="imgContainerBox">
-              <img class="imgBox" src={randomImage}
-            </div>`
+        $('#imageContainer').append(`<div class="imgContainerBox">
+              <img class="imgBox" src={randomImage}</div>`
         );
     });
 
